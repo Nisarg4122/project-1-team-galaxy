a97e04c629234d0ff48003e1d5edecf19c060429
CoreeJoseph <cajoseph@cpp.edu>
2025-03-13 13:29:47 -0700

    menu

--- game.py
+++ game.py
@@ -77,6 +77,37 @@ def save_inventory(inventory):
         json.dump(inventory, file)
     print("Inventory saved.")
 
+def play_game(player, galaxy):
+    while true:
+        print("\n--- Game Menu ---")
+        print("1. Show Inventory")
+        print("2. Explore Galaxy")
+        print("3.Save Game")
+        print("4. Save Inventory")
+        print("5. Exit to Main Menu")
+        choice = input("Choose an option: ")
+        if choice =="1":
+            player.show_inventory()
+        elif choice == "2":
+            galaxy. show_plantes()
+            try:
+                planet_choice = int(input("Select a plant to explore:  "))
+                if 1 <= planet_choice <= len(galaxy.planets):
+                    galaxy.planets[planet_choice = 1].explore(player)
+                else:
+                        print("\nInvalid planet selection.")
+            except ValueError:
+                    print("\nPlease enter a valid number.")
+        elif choice == "3":
+            save_character(player)
+        elif choice == "4":
+            save_inventory(player.to_dict())
+        elif choice == "5":
+            print("\nReturning to the main menu..")
+            break
+        else:
+            print("\nInvalid option. Try again.")
+            
 def main():
     print("Welcome to galaxy explore!")
     while true:
